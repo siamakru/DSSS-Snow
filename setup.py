73871fead70588875eecd60d0da7ea2a22441603
siamakru <siamakrouzmeh90@gmail.com>
2022-12-05 08:29:22 +0100

    Set-up file for package config

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
 
+setup(name="snowflake",
+      version="0.1",
+      author="DSSS",
+      author_email="siamak.rouzmeh@fau.de",
+      packages=find_packages(),
+      install_requires=["numpy", "turtles"]
+      )
